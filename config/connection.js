b62a94e11c30f6bb7263b17218a4ddaeb24e1214
Alexandra Prodan <alexandra.nanut@gmail.com>
2023-06-23 16:44:22 +1000

    new changes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/SocialNetworkDB',{
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetworkDB',{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 });
